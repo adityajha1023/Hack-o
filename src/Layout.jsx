2a1db05467c9b969e81ee689e0dce8405e3daa04
adityajha1023 <adityajha.1023@gmail.com>
2026-01-28 00:04:27 +0530

    new

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
-import Navigation from './blocks/Navigation.jsx'
-import Footer from './blocks/Footer.jsx'
-import { Outlet } from 'react-router-dom'
+import { useEffect } from "react";
+import Navigation from "./blocks/Navigation.jsx";
+import Footer from "./blocks/Footer.jsx";
+import { Outlet } from "react-router-dom";
+import Lenis from "@studio-freight/lenis";
 
 function Layout() {
-    return (
-        <div>
-            <Navigation />
-            <Outlet />
-            <Footer />
-        </div>
-    )
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      smoothTouch: false,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return (
+    <div>
+      <Navigation />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
 
-export default Layout
+export default Layout;
