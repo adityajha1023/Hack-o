ea21c648601f1b651ae9c531827a121dc1998af2
adityajha1023 <adityajha.1023@gmail.com>
2026-01-30 02:24:44 +0530

    asdf

--- src/blocks/HowItWorks.jsx
+++ src/blocks/HowItWorks.jsx
@@ -5,8 +5,8 @@ function HowItWorks() {
     return (
         <div className='flex flex-col justify-between items-start gap-8 px-20 py-10'>
             <p className='mt-16 font-bold text-red-700 uppercase'>how it works ?</p>
-            <div className='flex flex-row gap-4 flex-wrap'>
-                <div className='bg-white py-6 px-8 rounded-3xl min-h-[60vh] max-w-[30rem] flex flex-col justify-start items-start gap-6 shadow-md'>
+            <div className='flex flex-row justify-evenly items-center flex-wrap w-full'>
+                <div className='bg-white py-6 px-8 rounded-3xl min-h-[60vh] max-w-[25rem] flex flex-col justify-start items-start gap-6 shadow-md'>
                     <span className='flex flex-row gap-2 font-semibold text-p'>
                         <NotebookPen color='#FF3300' fill='#FFC2B3' />
                         Registration
@@ -14,7 +14,7 @@ function HowItWorks() {
                     <h3 className='text-h4 text-black/50 font-medium'>Students register in teams of 3–4 and submit basic project interests.</h3>
                     
                 </div>
-                <div className='bg-white py-6 px-8 rounded-3xl min-h-[60vh] max-w-[30rem] flex flex-col justify-start items-start gap-6 shadow-md'>
+                <div className='bg-white py-6 px-8 rounded-3xl min-h-[60vh] max-w-[25rem] flex flex-col justify-start items-start gap-6 shadow-md'>
                     <span className='flex flex-row gap-2 font-semibold text-p'>
                         <MousePointerClick color='#FF3300' fill='#FFC2B3' />
                         Shortlisting
@@ -22,7 +22,7 @@ function HowItWorks() {
                     <h3 className='text-h4 text-black/50 font-medium'>Top 80 teams are shortlisted based on project and intent.</h3>
                     
                 </div>
-                <div className='bg-white py-6 px-8 rounded-3xl min-h-[60vh] max-w-[30rem] flex flex-col justify-start items-start gap-6 shadow-md'>
+                <div className='bg-white py-6 px-8 rounded-3xl min-h-[60vh] max-w-[25rem] flex flex-col justify-start items-start gap-6 shadow-md'>
                     <span className='flex flex-row gap-2 font-semibold text-p'>
                         <Ribbon color='#FF3300' fill='#FFC2B3' />
                         Final Hackathon
