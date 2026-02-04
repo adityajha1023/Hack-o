92049f88ffc5006d2b2b8e1c29fb734c2587b90a
adityajha1023 <adityajha.1023@gmail.com>
2026-02-05 00:25:51 +0530

    upload all

--- src/blocks/Footer.jsx
+++ src/blocks/Footer.jsx
@@ -1,16 +1,40 @@
 import React from 'react'
 
 function Footer() {
-    return (
-        <div className='bg-[#000001] text-white py-2'>
-            <h2 className='flex justify-center items-center font-semibold text-white/20 text-[10rem] py-8'>Hack O' Clock</h2>
-            <div className='flex flex-row justify-between items-center text-p text-white/90 mx-6'>
-                <a href="#">About</a>
-                <a href="#">Judges</a>
-                <a href="#">Contact</a>
-            </div>
-        </div>
-    )
+  return (
+    <footer className="bg-[#000001] text-white">
+      
+      {/* Background Title */}
+      <h2
+        className="
+          text-white/20 font-semibold text-center
+          text-[3rem] sm:text-[4rem] md:text-[6rem] lg:text-[8rem]
+          leading-none
+          py-6 sm:py-8
+        "
+      >
+        Hack O&apos; Clock
+      </h2>
+
+      {/* Footer Links */}
+      <div
+        className="
+          flex flex-col sm:flex-row
+          justify-center sm:justify-between
+          items-center
+          gap-3 sm:gap-0
+          mx-6 pb-4
+          text-[0.833rem] sm:text-[1rem]
+          text-white/90
+        "
+      >
+        <a href="#" className="hover:text-white transition">About</a>
+        <a href="#" className="hover:text-white transition">Judges</a>
+        <a href="#" className="hover:text-white transition">Contact</a>
+      </div>
+
+    </footer>
+  )
 }
 
 export default Footer
